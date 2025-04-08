2487706b75db08298a3d4e6446c3571a898c78e5
rushishinde23 <123rishinde321@gmail.com>
2025-04-08 16:22:52 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.println("hello rushi this side");
+        System.out.println("This code build using jenkins pipeline");
+    }
+}
